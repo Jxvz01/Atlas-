f5bcaff0ea5b7a6539e8c738eef92a61ba8d66a1
Jxvz01 <jeevanh259@gmail.com>
2026-04-18 11:23:18 +0530

    Configure Node for cloud port

--- node_backend/server.js
+++ node_backend/server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 const DATA_DIR = path.join(__dirname, 'data');
 const DATA_FILE = path.join(DATA_DIR, 'telemetry_archive.json');
 
